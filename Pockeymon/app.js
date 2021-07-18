35ddcada66b704fa4fd6b3da18a2311cd9a5ae2c
Aayush Kumar <72338430+Aayush15011@users.noreply.github.com>
2021-07-18 17:03:49 +0530

    Add files via upload

---
+++ Pockeymon/app.js
@@ -0,0 +1,43 @@
+// getting element by it's Id
+const pokedex = document.getElementById('pokedex');
+
+const fetchPokemon = () => {
+    const promises = [];
+    for (let i = 1; i <= 150; i++) {
+
+        // fetching the url link 
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+
+    // using Promises 
+    Promise.all(promises).then((results) => {
+        //using map method to get data from result variable
+        const pokemon = results.map((result) => ({
+            name: result.name,
+            image: result.sprites['front_default'],
+            type: result.types.map((type) => type.type.name).join(', '),
+            id: result.id
+        }));
+        displayPokemon(pokemon);
+    });
+};
+
+//Displaying image , id , name and it's type 
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon
+        .map(
+            (pokeman) => `
+        <li class="card">
+            <img class="card-image" src="${pokeman.image}"/>
+            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+            <p class="card-subtitle">Type: ${pokeman.type}</p>
+        </li>
+    `
+        )
+        .join('');
+    pokedex.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon();
